1376e39de10a1cacf7b189f4b8711cff7111d6e8
Vitor Costa <73965917+vitorcostadev@users.noreply.github.com>
2026-06-27 18:57:35 -0300

    ok

---
+++ src/tests/JsonTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import domain.facade.JsonLocalFacade;
+import utils.JsonValue;
+import utils.Parser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class JsonTest {
+    static void main() throws IOException {
+        JsonLocalFacade json = new JsonLocalFacade();
+        List<String> strings = Parser.separarObjetos(
+                new String(Files.readAllBytes(Paths.get("league_data.json"))))
+                        .orElseThrow(RuntimeException::new);
+
+        JsonValue.JObject astRaiz = Parser.parseLeagueToAst(strings);
+
+        JsonValue jsonValue = astRaiz.asObject().get("1")
+                .asObject()
+                .get("time");
+
+        System.out.println(jsonValue);
+        JsonValue jsonValue1 = jsonValue.asObject()
+                .get("1")
+                .asObject()
+                .get("t1");
+
+        System.out.println(jsonValue1);
+    }
+
+
+
+}
---
+++ src/utils/JsonValue.java
@@ -0,0 +1,39 @@
+package utils;
+
+import java.util.List;
+import java.util.Map;
+
+public sealed interface JsonValue {
+
+    record JString(String value) implements JsonValue {}
+    record JNumber(int value) implements JsonValue {}
+    record JNull() implements JsonValue {}
+    record JObject(Map<String, JsonValue> fields) implements JsonValue {}
+    record JArray(List<JsonValue> elements) implements JsonValue {}
+
+    default String asString() {
+        if (this instanceof JString(String s)) return s;
+        throw new IllegalStateException("Violação de Tipo: Esperado JString, obtido " + this.getClass().getSimpleName());
+    }
+
+    default int asInt() {
+        if (this instanceof JNumber(int n)) return n;
+        throw new IllegalStateException("Violação de Tipo: Esperado JNumber, obtido " + this.getClass().getSimpleName());
+    }
+
+    default Map<String, JsonValue> asObject() {
+        if (this instanceof JObject(var map)) return map;
+        throw new IllegalStateException("Violação de Tipo: Esperado JObject, obtido " + this.getClass().getSimpleName());
+    }
+
+    default int size() {
+        return switch (this) {
+            case JObject(var map)   -> map.size();
+            case JArray(var list)   -> list.size();
+            case JString(var s)     -> s.length();
+            case JNumber _, JNull _ -> throw new UnsupportedOperationException(
+                    "Nós escalares (" + this.getClass().getSimpleName() + ") não possuem cardinalidade."
+            );
+        };
+    }
+}
